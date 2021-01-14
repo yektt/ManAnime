5314cfcc33c0e943353043ceae990cd5ea991370
yektt <yektaturan.93@gmail.com>
2021-01-14 17:56:32 +0100

    home_index_path changed as root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'home#index'
+
   get 'users/new'
 
   get 'users/show'
 
   get 'users/edit'
-
-  get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
