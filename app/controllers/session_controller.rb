6f37b6dce7f61a6509f3849181a7e450f6819f1a
yektt <yektaturan.93@gmail.com>
2021-01-18 16:50:54 +0100

    Created a create action in Session Model
    finding the user mail and comparing the passwords that user has entered and saved one in the application.
    
    if they match, user id will be asssign as session_id
    if not, login page will be rendered.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,15 @@
 class SessionController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email].downcase)
+
+    if(user.present? && user.authenticate(params[:password]))
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
 end
