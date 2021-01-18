6af3471515b4d7a3de2a165768298d0d605eed64
yektt <yektaturan.93@gmail.com>
2021-01-18 15:31:15 +0100

    Added password_digest attribute to the User Model
    - as a string attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210118101751) do
+ActiveRecord::Schema.define(version: 20210118142904) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
