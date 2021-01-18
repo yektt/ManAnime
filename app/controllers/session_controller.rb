20087af1848d62b193a09a806479a35fc05f7bfe
yektt <yektaturan.93@gmail.com>
2021-01-18 17:05:10 +0100

    Created an alert if user didn't filled the fields that necessary

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,14 +3,19 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email].downcase)
-
-    if(user.present? && user.authenticate(params[:password]))
-      session[:user_id] = user.id
-      redirect_to root_path
-    else
-      flash[:alert] = "Email or password is invalid.  Please try again"
+    if (params[:email].blank? || params[:password].blank?)
+      flash[:alert] = "Both areas should be filled!"
       render 'new'
+    else
+      user = User.find_by(email: params[:email].downcase)
+
+      if(user.present? && user.authenticate(params[:password]))
+        session[:user_id] = user.id
+        redirect_to root_path
+      else
+        flash[:alert] = "Email or password is invalid.  Please try again"
+        render 'new'
+      end
     end
   end
 end
