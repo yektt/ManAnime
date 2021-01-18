df850445a4ffb98fe5835cd2691e6dec1bd12416
yektt <yektaturan.93@gmail.com>
2021-01-18 15:58:18 +0100

    role attribute has been added to the User Model
    type:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210118142904) do
+ActiveRecord::Schema.define(version: 20210118145706) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "role"
   end
 
 end
