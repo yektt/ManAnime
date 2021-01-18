f1d1609115150b0712c5d9f8a7bb668522674103
yektt <yektaturan.93@gmail.com>
2021-01-18 11:18:46 +0100

    User model has been created
    -with email(string) attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20210118101751) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
