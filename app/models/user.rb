ec5d0104aefd506f7607c627308ef68b88290780
yektt <yektaturan.93@gmail.com>
2021-01-18 16:23:34 +0100

    Role attribute's default value assigned as 'registered'
    role attribute can only be these three; registered, admin, blocked.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
-class User < ApplicationRecord
-
+class User < ApplicationRecord  
   validates :email, presence: true, uniqueness: true
+  validates :role, inclusion: {in: %w(registered admin blocked)}
+
   has_secure_password
 
+  private
+
+  def default_role!
+    self.role ||= 'registered'
+  end
 end
