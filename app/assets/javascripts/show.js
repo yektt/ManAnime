a95fb71b89bd9e200fd0a615e1174efe873723a6
yektt <yektaturan.93@gmail.com>
2021-01-20 22:40:36 +0100

    Activated anime tab via JS

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,5 +1,6 @@
 if( window.location.href.includes('profile')) {
   document.addEventListener('DOMContentLoaded', function(){ 
+    document.getElementById('tab_anime').addEventListener("click", animeFunction);
     document.getElementById('tab_manga').addEventListener("click", mangaFunction);
   }, false);
   
@@ -26,5 +27,30 @@ if( window.location.href.includes('profile')) {
     document.getElementById('tab_manga').parentElement.classList.add('show');
     document.getElementById('tab_manga').parentElement.classList.add('active');
     document.getElementById('tab_manga').getAttribute("aria-selected") ==  true;
+  } 
+  
+  function animeFunction() {
+    var anime = document.getElementById('anime');
+    anime.classList.add("active");
+    anime.classList.add("show");
+    anime.classList.remove("fade");
+    
+    var children = anime.firstElementChild.children;
+    var i;
+    for (i = 0; i < children.length; i++) {
+      children[i].style.display = "block";
+    }
+    
+    var mangas = document.getElementById('manga');
+    mangas.classList.remove('show');
+    mangas.classList.remove('active');
+    mangas.classList.add('fade');
+    
+    document.getElementById('tab_manga').parentElement.classList.remove('show');
+    document.getElementById('tab_manga').parentElement.classList.remove('active');
+    document.getElementById('tab_manga').getAttribute("aria-selected") ==  false;
+    document.getElementById('tab_anime').parentElement.classList.add('show');
+    document.getElementById('tab_anime').parentElement.classList.add('active');
+    document.getElementById('tab_anime').getAttribute("aria-selected") ==  true;
   } 
 }
