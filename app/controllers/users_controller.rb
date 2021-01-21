b38416c30fc4f94612022ecdc57b887bb3f6cb4a
yektt <yektaturan.93@gmail.com>
2021-01-21 21:01:00 +0100

    Created update action for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,24 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(session[:user_id])
   end
 
   def edit
+    @user = User.find(session[:user_id])
+    logger.info('in edit')
+    logger.info(@user.id)
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+    logger.info('in update')
+
+    if @user.update(edit_user_params)
+      redirect_to profile_path
+    else
+      format.html { render :edit }
+    end
   end
 
   private
@@ -24,4 +39,5 @@ class UsersController < ApplicationController
   def new_user_params
     params.require(:user).permit(:email, :password)
   end
+
 end
