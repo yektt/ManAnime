2975f50ed1be69a8fd7136b87e9500153d0269cf
yektt <yektaturan.93@gmail.com>
2021-01-22 09:59:18 +0100

    Genre model has been created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210119171530) do
+ActiveRecord::Schema.define(version: 20210122085839) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
