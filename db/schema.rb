b6aead2e8291c61af909824d68f11fda289203e6
yektt <yektaturan.93@gmail.com>
2021-01-22 16:52:16 +0100

    Created character model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210122153434) do
+ActiveRecord::Schema.define(version: 20210122155106) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.text "information"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
