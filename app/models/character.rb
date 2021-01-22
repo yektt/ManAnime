7b42a1cc01eb2feccdb639989379166e7d0011ab
yektt <yektaturan.93@gmail.com>
2021-01-22 17:18:12 +0100

    Added rules for presence of character model's attribute
    -all attributes should be given

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,6 @@
 class Character < ApplicationRecord
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :avatar_url, presence: true
+  validates :information, presence: true
 end
