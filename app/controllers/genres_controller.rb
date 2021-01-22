1f8ca955312e63d16ad0c674a3b32a7ea0b18a9d
yektt <yektaturan.93@gmail.com>
2021-01-22 10:52:54 +0100

    Added genre - create action

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,4 +2,13 @@ class GenresController < ApplicationController
   def new
     @genre = Genre.new
   end
+
+  def create
+    @genre = Genre.new(new_genre_params)
+    if(@genre.save)
+      redirect_to add_path
+    else
+      render 'new'
+    end
+  end
 end
