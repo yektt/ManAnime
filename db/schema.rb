4d73062a99fc95420dc7bf075810d6f4ba94c9bb
yektt <yektaturan.93@gmail.com>
2021-01-22 15:28:25 +0100

    Extended user model with avatar attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210122085839) do
+ActiveRecord::Schema.define(version: 20210122140942) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20210122085839) do
     t.string "name"
     t.boolean "is_blocked"
     t.string "avatar_url"
+    t.string "avatar"
   end
 
 end
