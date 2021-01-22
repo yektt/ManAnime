4e80135d506144f3f1e8772c152531e9eab27a13
yektt <yektaturan.93@gmail.com>
2021-01-22 17:14:49 +0100

    Created new and create action for character controller
    -with strong parameter for creating a new character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,23 @@
 class CharactersController < ApplicationController
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(new_character_params)
+
+    if (@character.save)
+      flash[:notice] = "Successfully created!"
+      redirect_to add_path
+    else
+      flash[:alert] = "All areas should be filled!"
+      render 'new'
+    end
+  end
+
+  private
+
+  def new_character_params
+    params.require(:character).permit(:name, :surname, :avatar_url, :information)
+  end
 end
