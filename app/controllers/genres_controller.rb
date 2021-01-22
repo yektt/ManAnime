425024a752b776e99f992ca52b5198212c6fd3ec
yektt <yektaturan.93@gmail.com>
2021-01-22 11:32:49 +0100

    Updated genre - create action
    making three checks for rendering three flashes:
    -if genre has been added
    -if genre has less than two characters
    -if genre has been added successfully

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,9 +5,15 @@ class GenresController < ApplicationController
 
   def create
     @genre = Genre.new(new_genre_params)
-    if(@genre.save)
+
+    if (Genre.find_by(new_genre_params))
+      flash[:alert] = "This genre has been created already!"
+      render 'new'
+    elsif (@genre.save)
+      flash[:notice] = "Successfully created!"
       redirect_to add_path
     else
+      flash[:alert] = "Name area cannot be blank or less than two letters!"
       render 'new'
     end
   end
