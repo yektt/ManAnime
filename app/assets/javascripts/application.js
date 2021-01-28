b6ab3252e49a20e8c80af538d637d5451114adfc
yektt <yektaturan.93@gmail.com>
2021-01-28 18:10:35 +0100

    Wrote functions to move active class for header and footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,27 @@
 //= require_tree .
+
+document.addEventListener('DOMContentLoaded', function(){
+  var header = document.querySelectorAll('header a');
+  var footer = document.querySelectorAll('footer a');
+
+  remove_active_class(header);
+  remove_active_class(footer);
+  add_active_class(header);
+  add_active_class(footer);
+}, false);
+
+function remove_active_class(component) {
+  for (i = 0; i<component.length; i++) {
+    if(component[i].classList.contains('active')) {
+      component[i].classList.remove('active');
+    }
+  }
+}
+
+function add_active_class(component){
+  for (i = 1; i<component.length; i++) {
+    if(component[i].href == location.href && component[i]) {
+      component[i].classList.add('active');
+    }
+  }
+}
