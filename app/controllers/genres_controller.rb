3d30ed45f7ef4b8b0387540753604d39ea3320b6
yektt <yektaturan.93@gmail.com>
2021-02-01 22:16:43 +0100

    Refactored flash alerts in genre controller and view-new

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,15 +5,11 @@ class GenresController < ApplicationController
 
   def create
     @genre = Genre.new(new_genre_params)
-
-    if (Genre.find_by(new_genre_params))
-      flash[:alert] = "This genre has been created already!"
-      render 'new'
-    elsif (@genre.save)
+  
+    if (@genre.save)
       flash[:notice] = "Successfully created!"
       redirect_to add_path
     else
-      flash[:alert] = "Name area cannot be blank or less than two letters!"
       render 'new'
     end
   end
