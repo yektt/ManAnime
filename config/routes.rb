c856201c5e2b8f8e378e7eab524c0e15b89c83ff
yektt <yektaturan.93@gmail.com>
2021-02-01 22:05:46 +0100

    Refactored routes.rb
    more usage of resources

--- config/routes.rb
+++ config/routes.rb
@@ -17,21 +17,14 @@ Rails.application.routes.draw do
     get 'login', to: 'sessions#new', as: 'login'
 
     resources :users
-    get 'signup', to: 'users#new' 
-    post 'signup', to: 'users#create'
+    get 'signup', to: 'users#new', as: 'signup'
     get 'profile', to: 'users#show'
-    get 'edit', to: 'users#edit'
-    patch 'update', to: 'users#update'
+    get 'edit/:id', to: 'users#edit', as: 'edit'
 
     post 'block', to: 'users#block'
 
-    get 'add_genre', to: 'genres#new'
-    post 'add_genre', to: 'genres#create'
+    resources :genres, only: [:new, :create]
 
-    get 'add_character', to: 'characters#new'
-    post 'add_character', to: 'characters#create'
-    get '/:id/show_character', to:'characters#show', as:'show_character'
-    get '/:id/edit_character', to:'characters#edit', as:'edit_character'
-    patch '/:id/update_character', to:'characters#update', as:'update_character'
+    resources :characters, only: [:new, :create, :show, :edit, :update]
   end
 end
