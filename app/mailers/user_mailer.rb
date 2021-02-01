e72e8b1fbbcb65353ae84f306540b1d994329b4f
yektt <yektaturan.93@gmail.com>
2021-02-01 18:09:07 +0100

    Created welcoming action in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: 'mananimeteam@gmail.com'
+
+  def welcoming
+    @user = params[:user]
+    mail(to: @user.email, subject: 'Welcome')
+  end
 end
