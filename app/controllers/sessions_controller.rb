25fce35c6c31615ce1ad4d5765482812439a414f
yektt <yektaturan.93@gmail.com>
2021-02-01 15:54:28 +0100

    Changed the name of session controller to sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email].downcase)
+
+    if(user.present? && user.authenticate(params[:password]))
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:alert] = "Email or password is invalid.  Please try again"
+      render 'new'
+    end
+  end
+
+  def destroy
+    logger.info('in destroy')
+    session.delete(:user_id) 
+    redirect_to root_path
+  end
+end
