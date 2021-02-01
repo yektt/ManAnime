2e88481dd3e9e08e13a091d4c5922ce4df950cce
yektt <yektaturan.93@gmail.com>
2021-02-01 18:07:06 +0100

    Made the smtp settings for the UserMailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,18 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost:3000',
+    user_name:            'mananimeteam@gmail.com',
+    password:             'fUgpic-zeqpyx-hozju0',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 
   config.action_mailer.perform_caching = false
 
