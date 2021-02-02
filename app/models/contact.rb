1cdb1ca6c3327f03a123a4edaa61477ad37e35a0
yektt <yektaturan.93@gmail.com>
2021-02-02 11:07:41 +0100

    Created MailForm-contact model
    with mail, name, topic and message attributes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < MailForm::Base
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :topic,     :validate => true
+  attribute :name,      :validate => true
+  attribute :message,   :validate => true
+
+  def headers
+    {
+      :subject => "My Contact Form -  #{topic} ",
+      :to => "mananimeteam@gmail.com",
+      :from => %("#{email} - #{name}")
+    }
+  end
 end
