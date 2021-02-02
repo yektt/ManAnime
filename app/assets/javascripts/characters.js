3cd2774e69eec917d121632147bc232c1f9c9a47
yektt <yektaturan.93@gmail.com>
2021-02-02 11:29:53 +0100

    Fixed path for characters_show page in Character.js

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -1,4 +1,4 @@
-if( window.location.href.includes('show_character')) {
+if( window.location.href.includes('characters')) {
   document.addEventListener('DOMContentLoaded', function(){
     var special_info = document.getElementById('character_special');
     var text = special_info.textContent;
