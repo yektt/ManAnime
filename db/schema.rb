7c32c78968be33dab65ff4761d83aeeac4131516
yektt <yektaturan.93@gmail.com>
2021-02-02 22:56:39 +0100

    Created Anime Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210122160330) do
+ActiveRecord::Schema.define(version: 20210202212356) do
+
+  create_table "animes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_date"
+    t.string "link_to_watch"
+    t.text "description"
+    t.integer "season_number"
+    t.integer "episode_number"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -19,6 +31,14 @@ ActiveRecord::Schema.define(version: 20210122160330) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar_url"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "genres", force: :cascade do |t|
