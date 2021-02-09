44d69de00bfae7eb532ba0aaaa6b5e3c7562b016
yektt <yektaturan.93@gmail.com>
2021-02-09 17:49:19 +0100

    Created validations for Conent Model

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,10 @@
 class Content < ApplicationRecord
+  validates :name, presence: true
+  validates :category, presence: true, inclusion: {in: %w(anime manga)}
+  validates :tags, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+  validates :start_date, presence: true
+  validates :volume_or_season_number, presence: true
+  validates :episode_or_chapter_number, presence: true
 end
