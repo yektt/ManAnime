d707009b66d2d3482799cf121a159e139c2f7c2d
yektt <yektaturan.93@gmail.com>
2021-02-12 18:12:17 +0100

    Created create action in CategoriesController
    For adding selected genre to Content.categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,13 @@
 class CategoriesController < ApplicationController
+  def create
+    @content = Content.find(params[:content_id])
+    @genre = Genre.find(params[:genre_id])
+
+    if (@content.categories.exists?(@genre.id))
+      @content.categories.destroy(@genre)
+    else
+      @content.categories << @genre
+    end
+    redirect_to editing_genres_path(@content)
+  end
 end
