b1442a8d7d567396e6fdfc709ce073b11efe881e
yektt <yektaturan.93@gmail.com>
2021-02-17 17:18:18 +0100

    Deleted surname validation from character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,5 @@
 class Character < ApplicationRecord
   validates :name, presence: true
-  validates :surname, presence: true
   validates :avatar_url, presence: true
   validates :information, presence: true
 
