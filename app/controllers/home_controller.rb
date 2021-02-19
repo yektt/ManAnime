54249c5c7cca1fb04bd1a9a91d5ace968a34ca46
yektt <yektaturan.93@gmail.com>
2021-02-19 11:44:01 +0100

    Filled anime_list action in HomeController
    -all: will store all animes alphabetically
    -popular: will store most popular animes
    -recent: will store most recent animes
    -unfinished_popular: will store most popular but unfinished animes
    *Except 'all', each of them can be 5 at max

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,10 @@ class HomeController < ApplicationController
   end
 
   def anime_list
+    @all = Content.anime_list.alphabetical_order.page(params[:page])
+    @popular = Content.anime_list.most_popular
+    @recent = Content.anime_list.most_recent
+    @unfinished_popular = Content.anime_list.unfinished.most_popular
   end
   
   def manga_list
