2752fcebd63e02aa280d61f111e06da4c3b225ac
yektt <yektaturan.93@gmail.com>
2021-02-25 17:47:15 +0100

    Made the headers grammatically correct
    for pluralisation and usage of is/are

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def pluralize_contents(contents)
+    if ( contents.length > 1 )
+      contents.first.category + "s"
+    else
+      contents.first.category
+    end
+  end
+
+  def is_or_are(contents)
+    if ( contents.length > 1 )
+      "are"
+    else
+      "is"
+    end
+  end
 end
