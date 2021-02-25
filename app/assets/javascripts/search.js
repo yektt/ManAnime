48e17ac99678220b6dd6bbb6c5091bfb9dd3bef2
yektt <yektaturan.93@gmail.com>
2021-02-25 16:16:04 +0100

    Created displayNone function in search.js
    it will be activated in two conditions:
     1- if advanced search will be closed
     2- if dropdown menu has been hovered while advanced menu is open

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,7 @@
 if( window.location.href.includes('search')) {
   document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('advanced_search_button').addEventListener("click", changeDisplay);
+    document.getElementById('dropdown').addEventListener("mouseover", displayNone);
   }, false);
 }
 
@@ -11,7 +12,14 @@ function changeDisplay() {
     advanced_search_field.classList.remove('d-none');
     advanced_search_button.style.backgroundColor = '#ccd2ce';
     advanced_search_button.style.color = '#3f3445';
-  } else {
+  } else 
+    displayNone();
+}
+
+function displayNone() {
+  var advanced_search_field = document.getElementById('advanced_search');
+  var advanced_search_button = document.getElementById('advanced_search_button');
+  if(!advanced_search_field.classList.contains('d-none')) {
     advanced_search_field.classList.add('d-none');
     advanced_search_button.style.backgroundColor = 'transparent';
     advanced_search_button.style.color = '#6c757d';
