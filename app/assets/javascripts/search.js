de91de3b26bc1dccf3fb976c1cd0d42ebf61fa6b
yektt <yektaturan.93@gmail.com>
2021-02-25 16:19:11 +0100

    Checked the search_bar in search.js
    if there is any change (adding new character or deleting a current one), JS will render new result up to new entry
    with renewing result every time, search_bar will be focused and cursor will be at the end of the input

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,9 +1,44 @@
 if( window.location.href.includes('search')) {
   document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('search_bar').focus();
+    moveCursorToEnd(document.getElementById('search_bar'))
     document.getElementById('advanced_search_button').addEventListener("click", changeDisplay);
     document.getElementById('dropdown').addEventListener("mouseover", displayNone);
+    document.getElementById('search_bar').addEventListener("keyup",debounce(function(){ 
+      document.getElementById('submit_search_button').click();
+    }, 500));
   }, false);
 }
+
+function moveCursorToEnd(el) {
+  if (typeof el.selectionStart == "number") {
+      el.selectionStart = el.selectionEnd = el.value.length;
+  } else if (typeof el.createTextRange != "undefined") {
+      el.focus();
+      var range = el.createTextRange();
+      range.collapse(false);
+      range.select();
+  }
+}
+
+function debounce(func, wait, immediate) {
+  var timeout;
+
+  return function executedFunction() {
+    var context = this;
+    var args = arguments;
+        
+    var later = function() {
+    timeout = null;
+    if (!immediate) func.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+    };
+};
 
 function changeDisplay() {
   var advanced_search_field = document.getElementById('advanced_search');
