496f37d05f4de57a86db582c5a144ff1e7d8593a
yektt <yektaturan.93@gmail.com>
2021-02-26 17:03:12 +0100

    Created a check for blocked user in session controller
    -if user is not blocked, they can login without a problem.
    -if user is blocked, they will get an error to go to contact or help page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email].downcase)
 
-    if(user.present? && user.authenticate(params[:password]))
+    if(user.present? && user.authenticate(params[:password]) && user.is_blocked != true)
       session[:user_id] = user.id
       redirect_to root_path
     else
-      flash[:alert] = "Email or password is invalid.  Please try again"
+      if(user.is_blocked == true)
+        alert1 = ['You have been banned from the application!', '<br/>', 
+                  'Please check our rules from', "<a href=\"/help_and_rules\">Help & Rules Page.</a>", '<br/>',
+                  'If you think there is a problem, please contact with us via', "<a href=\"/contact\">Contact Page!</a>"]
+        flash[:alert] = alert1.join.html_safe
+      else
+        flash[:alert] = "Email or password is invalid.  Please try again"
+      end
       render 'new'
     end
   end
