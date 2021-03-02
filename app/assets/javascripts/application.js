ae9ba943ba821e385bd1a54174e758d2024bbf94
yektt <yektaturan.93@gmail.com>
2021-03-02 16:37:31 +0100

    Added changeTab to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,37 @@ function add_active_class(component){
     }
   }
 }
+
+function changeTab(will_be_active_tab_id,will_be_passive_tab_id, will_be_active_tab,  will_be_passive_tab ) {
+  var items_active = document.getElementById(will_be_active_tab_id);
+  items_active.classList.add("active");
+  items_active.classList.add("show");
+  items_active.classList.remove("fade");
+  
+  var items_passive = document.getElementById(will_be_passive_tab_id);
+  items_passive.classList.remove('show');
+  items_passive.classList.remove('active');
+  items_passive.classList.add('fade');
+  
+  var children = items_passive.firstElementChild.children;
+  var i;
+  for (i = 0; i < children.length; i++) {
+    children[i].style.display = "none";
+  }
+
+  var children = items_active.firstElementChild.children;
+  var i;
+  for (i = 0; i < children.length; i++) {
+    children[i].style.display = "inline";
+  }
+  console.log(will_be_active_tab);
+  var tab_passive = document.getElementById(will_be_passive_tab);
+  tab_passive.parentElement.classList.remove('show');
+  tab_passive.parentElement.classList.remove('active');
+  tab_passive.getAttribute("aria-selected") ==  false;
+  
+  var tab_active = document.getElementById(will_be_active_tab)
+  tab_active.parentElement.classList.add('show');
+  tab_active.parentElement.classList.add('active');
+  tab_active.getAttribute("aria-selected") ==  true;
+}
