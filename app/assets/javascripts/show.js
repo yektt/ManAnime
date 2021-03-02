820d5595d10b1091f2ac74f0e2f7a105d64bf619
yektt <yektaturan.93@gmail.com>
2021-03-02 16:37:52 +0100

    Used changaTab function in show.js
    -for profile tabs

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,56 +1,14 @@
 if( window.location.href.includes('users')) {
   document.addEventListener('DOMContentLoaded', function(){ 
-    document.getElementById('tab_anime').addEventListener("click", animeFunction);
-    document.getElementById('tab_manga').addEventListener("click", mangaFunction);
+    document.getElementById('tab_anime').addEventListener("click", activateAnimeTab);
+    document.getElementById('tab_manga').addEventListener("click", activateMangaTab);
   }, false);
-  
-  function mangaFunction() {
-    var mangas = document.getElementById('manga');
-    mangas.classList.add("active");
-    mangas.classList.add("show");
-    mangas.classList.remove("fade");
-    
-    var anime = document.getElementById('anime');
-    anime.classList.remove('show');
-    anime.classList.remove('active');
-    anime.classList.add('fade');
-    
-    var children = anime.firstElementChild.children;
-    var i;
-    for (i = 0; i < children.length; i++) {
-      children[i].style.display = "none";
-    }
-    
-    document.getElementById('tab_anime').parentElement.classList.remove('show');
-    document.getElementById('tab_anime').parentElement.classList.remove('active');
-    document.getElementById('tab_anime').getAttribute("aria-selected") ==  false;
-    document.getElementById('tab_manga').parentElement.classList.add('show');
-    document.getElementById('tab_manga').parentElement.classList.add('active');
-    document.getElementById('tab_manga').getAttribute("aria-selected") ==  true;
-  } 
-  
-  function animeFunction() {
-    var anime = document.getElementById('anime');
-    anime.classList.add("active");
-    anime.classList.add("show");
-    anime.classList.remove("fade");
-    
-    var children = anime.firstElementChild.children;
-    var i;
-    for (i = 0; i < children.length; i++) {
-      children[i].style.display = "block";
-    }
-    
-    var mangas = document.getElementById('manga');
-    mangas.classList.remove('show');
-    mangas.classList.remove('active');
-    mangas.classList.add('fade');
-    
-    document.getElementById('tab_manga').parentElement.classList.remove('show');
-    document.getElementById('tab_manga').parentElement.classList.remove('active');
-    document.getElementById('tab_manga').getAttribute("aria-selected") ==  false;
-    document.getElementById('tab_anime').parentElement.classList.add('show');
-    document.getElementById('tab_anime').parentElement.classList.add('active');
-    document.getElementById('tab_anime').getAttribute("aria-selected") ==  true;
-  } 
+}
+
+function activateMangaTab() {
+  changeTab('manga', 'anime', 'tab_manga', 'tab_anime');
+}
+
+function activateAnimeTab() {
+  changeTab('anime', 'manga', 'tab_anime', 'tab_manga');
 }
