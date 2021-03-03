8bfaedec30aafe9b829d6dbd279942f3589ecacb
yektt <yektaturan.93@gmail.com>
2021-03-03 10:06:58 +0100

    Filled create action in CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,13 @@
 class CommentsController < ApplicationController
   def create
+    @content = Content.find(params[:content_id])
+    @comment = Comment.new(comment_params)
+    @comment.content = @content
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to @content
+    end
   end
 
   private
