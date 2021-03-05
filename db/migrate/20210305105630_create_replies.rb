477dab4551e4417ccaa07282cb42574645162a48
yektt <yektaturan.93@gmail.com>
2021-03-05 14:08:44 +0100

    Created replies model
    with an association with comment and user

---
+++ db/migrate/20210305105630_create_replies.rb
@@ -0,0 +1,10 @@
+class CreateReplies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :replies do |t|
+      t.string :reply_body
+      t.references :comment, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210302171650) do
+ActiveRecord::Schema.define(version: 20210305111135) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -62,6 +62,15 @@ ActiveRecord::Schema.define(version: 20210302171650) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.string "reply_body"
+    t.integer "comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["comment_id"], name: "index_replies_on_comment_id"
   end
 
   create_table "users", force: :cascade do |t|
