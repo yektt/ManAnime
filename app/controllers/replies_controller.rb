9c322c003dc7bbf29a3b15319330b65508214713
yektt <yektaturan.93@gmail.com>
2021-03-05 15:49:38 +0100

    Created create action in RepliesController
    with strong parameters

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,2 +1,20 @@
 class RepliesController < ApplicationController
+  def create
+    @content = Content.find(params[:content_id])
+    @comment = Comment.find(params[:comment_id])
+    @reply = Reply.new(reply_params)
+
+    @reply.user = current_user
+    @reply.comment = @comment
+
+    if @reply.save
+      redirect_to @content 
+    end
+  end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:reply_body)
+  end
 end
