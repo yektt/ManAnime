3e5fe4b4529c20881c2894b1d8a967283ac9ed91
yektt <yektaturan.93@gmail.com>
2021-03-10 17:41:22 +0100

    Filled create action in ReviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,21 @@
 class ReviewsController < ApplicationController
   def create
+    logger.info('in reviews create')
+    @content = Content.find(params[:content_id])
+    @review = Review.new(review_params)
+    @review.content = @content
+    @review.user = current_user
+
+    if @review.save
+      redirect_to @content 
+    end
   end
 
   def destroy
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:review_body, :rating)
   end
 end
