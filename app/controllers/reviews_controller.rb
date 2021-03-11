e46732660e71a92a46d70583c07e274bc0202d02
yektt <yektaturan.93@gmail.com>
2021-03-11 14:44:15 +0100

    Added categories to created model in reviewsController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,9 +2,21 @@ class ReviewsController < ApplicationController
   def create
     @content = Content.find(params[:content_id])
     @review = Review.new(review_params)
+
     @review.content = @content
     @review.user = current_user
 
+    review_categories = params[:categories].first
+    if (params[:categories].length == 1)
+      @review.categories = review_categories
+    else
+      params[:categories].drop(1).each do |category|
+        review_categories = review_categories + ', '
+        review_categories += category
+      end
+      @review.categories = review_categories
+    end
+    
     if @review.save
       redirect_to @content 
     end
