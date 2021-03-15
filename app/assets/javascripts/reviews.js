35b12316a4ccc76a9411b89ae810868471386f1a
yektt <yektaturan.93@gmail.com>
2021-03-15 16:52:28 +0100

    Created destroyReview function in reviews.js

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,2 +1,13 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+Reviews = {};
+
+Reviews.destroyReview = function(reviewId, contentName, length) {
+  review = document.getElementById("review" + reviewId);
+  if( length == 0) {
+    var no_reviews_added = document.createElement('h4');
+    no_reviews_added.className = "mt-3";
+    no_reviews_added.appendChild(document.createTextNode('No review has been added to ' + contentName +  ' so far!'));
+    review.parentElement.insertBefore(no_reviews_added, review.parentElement.firstChild);
+  }
+  review.nextElementSibling.remove();
+  review.remove();
+}
