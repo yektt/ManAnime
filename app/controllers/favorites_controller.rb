b617ad063eacf616bbadc0d2697077acd7f8fb3c
yektt <yektaturan.93@gmail.com>
2021-03-17 12:01:02 +0100

    Added create action to FavoritesController

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,8 @@
 class FavoritesController < ApplicationController
+  def create
+    user = current_user
+    content = Content.find(params[:content_id])
+    user.favorites << content
+    redirect_to(content)
+  end 
 end
