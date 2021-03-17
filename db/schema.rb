540c656baa1bb02e4771d21983d9cca2c1058088
yektt <yektaturan.93@gmail.com>
2021-03-17 10:56:59 +0100

    Added user_id and content_id to reviews model
    for being able to relate them easily

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210310152842) do
+ActiveRecord::Schema.define(version: 20210310155729) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -79,6 +79,8 @@ ActiveRecord::Schema.define(version: 20210310152842) do
     t.string "categories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "content_id"
   end
 
   create_table "users", force: :cascade do |t|
