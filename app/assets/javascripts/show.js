271a949f74f8f3afc1937fb65c5ab4ca14332061
yektt <yektaturan.93@gmail.com>
2021-03-19 17:42:34 +0100

    Activated modal with JS in show.js
    -will be used for sending blocking reason to the blocked user

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -2,6 +2,14 @@ if( window.location.href.includes('users')) {
   document.addEventListener('DOMContentLoaded', function(){ 
     document.getElementById('tab_anime').addEventListener("click", activateAnimeTab);
     document.getElementById('tab_manga').addEventListener("click", activateMangaTab);
+    if( window.location.href.includes('users')) {
+      let modal_button = document.getElementById('reason_modal');
+      console.log(modal_button);
+      document.getElementById('block_reason').addEventListener("click", function() {
+        modal_button.classList.add('show');
+        modal_button.style.display = 'inline';
+      })
+    }
   }, false);
 }
 
