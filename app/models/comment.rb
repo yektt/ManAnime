da58fca6f287c98c1cd133b7e84691eae7e02df6
yektt <yektaturan.93@gmail.com>
2021-03-19 17:49:04 +0100

    Added dependent: destroy to comment model
    if a user will be deleted, which is not allowed by application, the user's comments will be deleted as well

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   has_many :replies, dependent: :destroy
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, dependent: :destroy
 
   validates :comment_body, presence: true
 end
