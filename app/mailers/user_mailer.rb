fd84ce07f6ef085e26faa2d1a7b5414bb3a76739
yektt <yektaturan.93@gmail.com>
2021-03-23 12:06:44 +0100

    Created block_mail in user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,6 +6,13 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Welcome')
   end
 
+  def block_mail(user, reason)
+    @reason = reason
+    @user = user
+    logger.info(@user.name)
+    mail(to: @user.email, subject: 'You have been blocked')
+  end
+
   def receive_contact
     @contact = params[:contact]
     mail(to: @contact.email, subject: 'Contact Form')
