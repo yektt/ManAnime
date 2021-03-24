fbfdfda985d8a19e81a8559f2f69aa8b8ceef07e
yektt <yektaturan.93@gmail.com>
2021-03-24 18:31:05 +0100

    Created upvotes and downvotes scopes in vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :comment
   has_and_belongs_to_many :users
+
+  scope :upvotes, -> { where(vote_type: 'up') }
+  scope :downvotes, -> { where(vote_type: 'down') }
 end
