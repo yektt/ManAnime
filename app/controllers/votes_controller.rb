12037f35275aadb51647a37b33345878eda16d37
yektt <yektaturan.93@gmail.com>
2021-03-24 17:11:44 +0100

    Created create action in votesController

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,11 @@
 class VotesController < ApplicationController
+  def create
+    @vote = Vote.new(vote_type: params[:vote_type])
+    @vote.comment_id = params[:comment_id]
+    
+    if(@vote.save)
+      @comment = Comment.find(params[:comment_id])
+      @comment.votes << @vote
+    end
+  end
 end
