0d63a2442284b6e93f153fe53aa0c8dac52c9bd6
yektt <yektaturan.93@gmail.com>
2021-03-25 14:45:32 +0100

    Fixed user and vote models association

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :comment
-  has_and_belongs_to_many :users
+  belongs_to :user
 
   scope :upvotes, -> { where(vote_type: 'up') }
   scope :downvotes, -> { where(vote_type: 'down') }
