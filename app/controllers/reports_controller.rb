d5112beb250ff9e4d1c0252a1e0e58e358581f3e
yektt <yektaturan.93@gmail.com>
2021-04-01 17:49:40 +0200

    Added find_comment helper to commentsController

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
+  before_action :find_comment
   def create
-    comment = Comment.find(params[:comment_id])
     user = current_user
     user.reports << comment
     redirect_to(comment.content)
@@ -8,9 +8,14 @@ class ReportsController < ApplicationController
   
   def destroy
     user = User.find(params[:user_id])
-    comment = Comment.find(params[:id])
 
     user.reports.delete(comment)
     redirect_to(reports_path)
+  end
+
+  private
+
+  def find_comment
+    comment = Comment.find(params[:id])
   end
 end
