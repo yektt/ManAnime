5a6131b2736d34d180fb7b7b565c4e71d5e65bdf
yektt <yektaturan.93@gmail.com>
2021-04-01 17:11:57 +0200

    Carried edit and update actions to the accountController
    from userController

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,2 +1,26 @@
 class AccountController < ApplicationController
+  before_action :ensure_pageowner , only: :edit
+
+  def edit
+    @user = User.find(params[:id])
+    logger.info('in account edit')
+    logger.info(@user.id)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    logger.info(params[:user_id])
+
+    if @user.update(edit_user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def edit_user_params
+    params.require(:user).permit(:name, :avatar)
+  end
 end
