0f9076494e63b3e7a414a82b215deb769379f790
yektt <yektaturan.93@gmail.com>
2021-04-01 17:50:15 +0200

    Rewrote sessionsController - create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,21 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email].downcase)
 
-    if(user.present? && user.authenticate(params[:password]) && user.is_blocked != true)
-      session[:user_id] = user.id
-      redirect_to root_path
-    else
-      if(user.is_blocked == true)
+    if(user.present? && user.authenticate(params[:password]))
+      if (user.is_blocked == false)
+        session[:user_id] = user.id
+        redirect_to root_path
+        return
+      elsif (user.is_blocked == true ) 
         alert1 = ['You have been banned from the application!', '<br/>', 
-                  'Please check our rules from', "<a href=\"/" + params[:locale] + "/help_and_rules\">Help & Rules Page.</a>", '<br/>',
-                  'If you think there is a problem, please contact with us via', "<a href=\"/" + params[:locale] + "/contact\">Contact Page!</a>" ]
+          'Please check our rules from', "<a href=\"/" + params[:locale] + "/help_and_rules\">Help & Rules Page.</a>", '<br/>',
+          'If you think there is a problem, please contact with us via', "<a href=\"/" + params[:locale] + "/contact\">Contact Page!</a>" ]
         flash[:alert] = alert1.join.html_safe
-      else
-        flash[:alert] = "Email or password is invalid.  Please try again"
       end
-      render 'new'
+    else
+      flash[:alert] = "Email or password is invalid.  Please try again"
     end
+    render 'new'
   end
 
   def destroy
