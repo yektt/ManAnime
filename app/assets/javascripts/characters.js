82fa271d3d880a4935fffd45fe8069b087eb15a8
yektt <yektaturan.93@gmail.com>
2021-04-01 17:51:48 +0200

    Added a check to character.js
    otherwise it was working for editing character view as well

--- app/assets/javascripts/characters.js
+++ app/assets/javascripts/characters.js
@@ -1,4 +1,4 @@
-if( window.location.href.includes('characters') && !window.location.href.includes('new')) {
+if( window.location.href.includes('characters') && !window.location.href.includes('new') && !window.location.href.includes('edit')) {
   document.addEventListener('DOMContentLoaded', function(){
     var special_info = document.getElementById('character_special');
     var text = special_info.textContent;
