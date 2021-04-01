12fc55298b31face49699e8e290068bc76e159a9
yektt <yektaturan.93@gmail.com>
2021-04-01 15:56:32 +0200

    Added authorisation & authentication rules to applicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :logged_in?, :current_user, :is_admin?, :all_reports, :did_user_voted_this_comment
+  helper_method :logged_in?, :current_user, :is_admin?, :all_reports, :ensure_owner, :ensure_authenticated, :ensure_admin
 
   before_action :set_locale
 
@@ -19,6 +19,19 @@ class ApplicationController < ActionController::Base
   def is_admin?
     return false unless(logged_in?)
     true if(current_user.role == 'admin')
+  end
+
+  def ensure_admin
+    redirect_to contact_path unless (is_admin?)
+  end
+
+  def ensure_authenticated
+    redirect_to login_path unless (logged_in?)
+  end
+
+  def ensure_owner 
+    comment = Comment.find(params[:comment_id])  
+    return true if (current_user == comment.user)
   end
 
   def default_url_options
