f598f110a81df49838feb22f1cce62bda98c96b5
yektt <yektaturan.93@gmail.com>
2021-04-12 11:49:47 +0200

    Added test for creating character without surname in character m-test

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -32,4 +32,14 @@ class CharacterTest < ActiveSupport::TestCase
 
     refute character.valid?
   end
+
+  test 'Creating a character without surname' do
+    character = Character.new name:'Name',
+                              information:'+Gender +Male +Eyes Color +Blue +When he went to the forest without letting know his parents.....', 
+                              avatar_url: 'https://i.pinimg.com/474x/cb/1c/bd/cb1cbd7a68a2e2ffdad62073d0eb5bd2.jpg'
+
+    character.save!
+
+    assert_equal character, Character.last
+  end
 end
