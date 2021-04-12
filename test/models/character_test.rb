76ae8caa2e361fdf91c24a372e57de7c70d7b3d4
yektt <yektaturan.93@gmail.com>
2021-04-12 11:45:48 +0200

    Created 'creating character' test in character model test

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class CharacterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Creating a character' do
+    character = Character.new name:'Name', surname:'Surname', 
+                              information:'+Gender +Male +Eyes Color +Blue +When he went to the forest without letting know his parents.....', 
+                              avatar_url: 'https://i.pinimg.com/474x/cb/1c/bd/cb1cbd7a68a2e2ffdad62073d0eb5bd2.jpg'
+
+    character.save!
+
+    assert_equal character, Character.last
+  end
 end
