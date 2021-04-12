46d37eead40b167a4ff0e6577d485408f5587d21
yektt <yektaturan.93@gmail.com>
2021-04-12 11:38:28 +0200

    Added downcase validation for name to genre model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,12 @@ class Genre < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { minimum: 2 }
 
   has_and_belongs_to_many :contents
+
+  before_validation :downcase_name
+  
+  private
+
+  def downcase_name
+    self.name = name.downcase
+  end
 end
