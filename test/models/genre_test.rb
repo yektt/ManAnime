f097e84bad176495625a7a794ed4531f963a2ff3
yektt <yektaturan.93@gmail.com>
2021-04-12 11:39:59 +0200

    Added 'adding existing genre' in genre model test

--- test/models/genre_test.rb
+++ test/models/genre_test.rb
@@ -7,4 +7,12 @@ class GenreTest < ActiveSupport::TestCase
 
     assert_equal genre, Genre.last
   end
+
+  test 'adding existing genre' do
+    genre = Genre.new name:'genre'
+    genre.save!
+
+    new_genre = Genre.new name:'genre'
+    refute new_genre.valid?
+  end
 end
