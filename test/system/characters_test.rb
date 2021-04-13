83fda4235a7a97aeb94a4b935b101ecf191fca27
yektt <yektaturan.93@gmail.com>
2021-04-13 17:25:20 +0200

    Created test to check validations for character in character s-test

--- test/system/characters_test.rb
+++ test/system/characters_test.rb
@@ -19,4 +19,21 @@ class CharactersTest < ApplicationSystemTestCase
 
     assert_equal current_path, character_path(:en, Character.last)
   end
+
+  test 'adding character - without necessary attirbutes' do
+    user = User.new email:'admin@mail.com', role: 'admin', password:'pass', name: 'name'
+    user.save!
+    
+    visit(login_path(:en))
+    fill_in(:email, with: user.email)
+    fill_in(:password, with: user.password)
+    find(:button, 'Log in').click
+
+    visit(new_character_path(:en))
+    find('[name=commit]').click
+
+    assert page.has_content?("Name can't be blank")
+    assert page.has_content?("Avatar url can't be blank")
+    assert page.has_content?("Information can't be blank")
+  end
 end
