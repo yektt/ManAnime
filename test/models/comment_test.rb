c9e5229a44b72c9da779baf43c481a36de18d5ca
yektt <yektaturan.93@gmail.com>
2021-04-13 14:55:02 +0200

    Created a test for editing existing comment in comment m-test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -45,5 +45,24 @@ class CommentTest < ActiveSupport::TestCase
     assert_equal content.comments.first, comment1
     assert_equal content.comments.last, comment3
   end
-  
+
+  test 'editing an existing comment' do
+    content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
+                      tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
+                      image:'https://pbs.twimg.com/media/EGUS_PzWwAAStTa.jpg',
+                      volume_or_season_number:2, episode_or_chapter_number: 35
+    content.save!
+
+    user = User.new email:'test@mail.com', name:'Test user', password:'password'
+    user.save!
+
+    comment = Comment.new comment_body:'Comment body here', user: user, content: content
+    comment.save!
+
+    assert_equal comment.comment_body, 'Comment body here'
+
+    comment.update(comment_body:'Comment body there')
+    assert_equal comment.comment_body, 'Comment body there'
+    refute_equal comment.comment_body, 'Comment body here'
+  end
 end
