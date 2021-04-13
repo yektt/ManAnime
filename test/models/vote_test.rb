fbef0ec86c828b7e9e7ff511da4c4c77e83b30a9
yektt <yektaturan.93@gmail.com>
2021-04-13 16:54:16 +0200

    Created a test for creating a downvote for comment in vote m-test

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -22,4 +22,26 @@ class VoteTest < ActiveSupport::TestCase
 
     assert_equal comment.votes.length, 1
   end
+
+  test 'creating a downvote for a comment' do
+    content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
+                      tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
+                      image:'https://pbs.twimg.com/media/EGUS_PzWwAAStTa.jpg',
+                      volume_or_season_number:2, episode_or_chapter_number: 35
+    content.save!
+
+    user1 = User.new email:'test1@mail.com', name:'Test user', password:'password'
+    user1.save!
+
+    comment = Comment.new comment_body:'Comment body here', user: user1, content: content
+    comment.save!
+
+    user2 = User.new email:'test2@mail.com', name:'Test user', password:'password'
+    user2.save!
+
+    vote = Vote.new vote_type:'down', comment: comment, user: user2
+    vote.save!
+
+    assert_equal comment.votes.length, 1
+  end
 end
