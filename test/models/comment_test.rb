f7caaeb03f179a3551761b6352675d9f06b57ecb
yektt <yektaturan.93@gmail.com>
2021-04-13 17:07:53 +0200

    Added a test to create a reported comment in comment m-test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -108,4 +108,25 @@ class CommentTest < ActiveSupport::TestCase
 
     assert_equal content.reload.comments.length, 2
   end
+
+  test 'reporting a comment' do
+    content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
+                      tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
+                      image:'https://pbs.twimg.com/media/EGUS_PzWwAAStTa.jpg',
+                      volume_or_season_number:2, episode_or_chapter_number: 35
+    content.save!
+
+    user = User.new email:'test@mail.com', name:'Test user', password:'password'
+    user.save!
+
+    comment = Comment.new comment_body:'Comment body here', user: user, content: content
+    comment.save!
+
+    assert_equal content.comments.length, 1
+    assert_equal user.reports.length, 0
+
+    user.reports << comment
+
+    assert_equal user.reload.reports.length, 1
+  end
 end
