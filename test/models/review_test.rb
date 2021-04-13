000cbec75892607d1c9a01ba674d23b3f891b906
yektt <yektaturan.93@gmail.com>
2021-04-13 16:19:20 +0200

    Created a test for creating a review in review m-test

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'creating review with rating' do
+    content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
+                      tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
+                      image:'https://pbs.twimg.com/media/EGUS_PzWwAAStTa.jpg',
+                      volume_or_season_number:2, episode_or_chapter_number: 35
+    content.save!
+
+    user = User.new email:'test@mail.com', name:'Test user', password:'password'
+    user.save!
+
+    assert_equal content.reviews.length, 0
+    assert content.reviews.first.nil?
+
+    review = Review.new review_body:'Review body here', rating: 8, 
+                        categories:'General', user: user, content: content
+    review.save!
+
+    assert_equal content.reviews.reload.length, 1
+    assert_equal content.reviews.first.rating, 8
+  end
 end
