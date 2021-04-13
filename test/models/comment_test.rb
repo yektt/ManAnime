dd618a5e8a844a9e60e303a5587934b4d74379f7
yektt <yektaturan.93@gmail.com>
2021-04-13 14:55:21 +0200

    Created a test to delete a comment in comment m-test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -65,4 +65,33 @@ class CommentTest < ActiveSupport::TestCase
     assert_equal comment.comment_body, 'Comment body there'
     refute_equal comment.comment_body, 'Comment body here'
   end
+
+  test 'deleting a comment' do
+    content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
+                      tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
+                      image:'https://pbs.twimg.com/media/EGUS_PzWwAAStTa.jpg',
+                      volume_or_season_number:2, episode_or_chapter_number: 35
+    content.save!
+
+    user1 = User.new email:'test1@mail.com', name:'Test user1', password:'password'
+    user1.save!
+
+    user2 = User.new email:'test2@mail.com', name:'Test user2', password:'password'
+    user2.save!
+
+    comment1 = Comment.new comment_body:'Comment body here', user: user1, content: content
+    comment1.save!
+
+    comment2 = Comment.new comment_body:'Comment body here', user: user2, content: content
+    comment2.save!
+
+    comment3 = Comment.new comment_body:'Comment body here', user: user1, content: content
+    comment3.save!
+
+    assert_equal content.comments.length, 3
+
+    comment1.destroy!
+
+    assert_equal content.reload.comments.length, 2
+  end
 end
