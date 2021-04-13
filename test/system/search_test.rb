877607fd052270e91229e067dc417cfb76863bbf
yektt <yektaturan.93@gmail.com>
2021-04-13 17:15:32 +0200

    Created tests to use advanced search in search s-test
    In test cases,
    - only one genre chosen
    - two genres chosen, no result
    - two genres chosen, with result
    - only starting year chosen
    - starting year & genre chosen

--- test/system/search_test.rb
+++ test/system/search_test.rb
@@ -26,4 +26,61 @@ class SearchTest < ApplicationSystemTestCase
     assert page.has_content?('Violet Evergarden')
   end
 
+  test 'advanced search - one genre choosen' do
+    visit(search_path(:en))
+    click_on('Advanced Search')
+    find(:css, "#genres_id_[value='2']").set(true)
+    within("#advanced_search") do
+      click_on('Search', match: :first)
+    end
+    assert page.has_content?('Adventure')
+  end
+
+  test 'advanced search - two genres choosen - no result' do
+    visit(search_path(:en))
+    click_on('Advanced Search')
+    find(:css, "#genres_id_[value='2']").set(true)
+    find(:css, "#genres_id_[value='8']").set(true)
+    within("#advanced_search") do
+      click_on('Search', match: :first)
+    end
+    assert page.has_content?('Adventure and Psychological')
+  end
+
+  test 'advanced search - two genres choosen - with result' do
+    visit(search_path(:en))
+    click_on('Advanced Search')
+    find(:css, "#genres_id_[value='2']").set(true)
+    find(:css, "#genres_id_[value='7']").set(true)
+    within("#advanced_search") do
+      click_on('Search', match: :first)
+    end
+    assert page.has_content?('Adventure and Mystery')
+  end
+
+  test 'advanced search - only start date choosen' do
+    visit(search_path(:en))
+    click_on('Advanced Search')
+    find(:css, "#year_2010").set(true)
+    within("#advanced_search") do
+      click_on('Search', match: :first)
+    end
+    assert_equal all(".grid-item").length, Content.where("CAST(strftime('%Y', start_date) as INT) >= ?", 2010).length
+    assert page.has_content?('2010')
+  end
+
+  test 'advanced search - date and genre choosen' do
+    visit(search_path(:en))
+    click_on('Advanced Search')
+
+    find(:css, "#year_2010").set(true)
+    find(:css, "#genres_id_[value='2']").set(true)
+    find(:css, "#genres_id_[value='7']").set(true)
+
+    within("#advanced_search") do
+      click_on('Search', match: :first)
+    end
+
+    assert page.has_content?('2010 for Adventure and Mystery')
+  end
 end
