aaf10e983cf5ab31f83ac23f41f02ef79074359c
yektt <yektaturan.93@gmail.com>
2021-04-13 16:21:24 +0200

    Created a test for creating review w/o categories in review m-test

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -21,4 +21,18 @@ class ReviewTest < ActiveSupport::TestCase
     assert_equal content.reviews.reload.length, 1
     assert_equal content.reviews.first.rating, 8
   end
+
+  test 'creating review without categories' do
+    content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
+                      tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
+                      image:'https://pbs.twimg.com/media/EGUS_PzWwAAStTa.jpg',
+                      volume_or_season_number:2, episode_or_chapter_number: 35
+    content.save!
+
+    user = User.new email:'test@mail.com', name:'Test user', password:'password'
+    user.save!
+
+    review = Review.new review_body:'Review body here', rating: 8, user: user, content: content
+    refute review.valid?
+  end
 end
