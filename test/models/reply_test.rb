16770f7d6e84728cdd2cb547519d3e0bd6c94e5c
yektt <yektaturan.93@gmail.com>
2021-04-13 14:53:37 +0200

    Created a test to deleting a reply in reply m-test

--- test/models/reply_test.rb
+++ test/models/reply_test.rb
@@ -36,4 +36,26 @@ class ReplyTest < ActiveSupport::TestCase
     reply = Reply.new user: user, comment: comment
     refute reply.valid?
   end
+
+  test 'deleting a reply' do
+    content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
+                      tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
+                      image:'https://pbs.twimg.com/media/EGUS_PzWwAAStTa.jpg',
+                      volume_or_season_number:2, episode_or_chapter_number: 35
+    content.save!
+
+    user = User.new email:'test@mail.com', name:'Test user', password:'password'
+    user.save!
+
+    comment = Comment.new comment_body:'Comment body here', user: user, content: content
+    comment.save!
+
+    reply = Reply.new reply_body:'Reply body here', user: user, comment: comment
+    reply.save!
+
+
+    reply.destroy!
+    content.reload.comments.length
+    assert_equal comment.reload.replies.length, 0
+  end
 end
