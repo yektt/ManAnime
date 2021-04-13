b6eb910bebc3c5401fa09c5691d03d6a98ea5d33
yektt <yektaturan.93@gmail.com>
2021-04-13 17:13:29 +0200

    Created tests to search content through app in search s-test

--- test/system/search_test.rb
+++ test/system/search_test.rb
@@ -1,5 +1,19 @@
 require "application_system_test_case"
 
 class SearchTest < ApplicationSystemTestCase
+
+  test 'search with results' do
+    visit(search_path(:en))
+    fill_in('q', with:'shi')
+
+    assert page.has_content?('Shigatsu wa Kimi no Uso')
+    refute page.has_content?('InuYasha')
+  end
   
+  test 'search without results' do
+    visit(search_path(:en))
+    fill_in('q', with:'abc')
+
+    refute page.has_content?("I couldn't find 'abc' in anywhere! ")
+  end
 end
