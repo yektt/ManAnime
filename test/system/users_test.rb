3325dc40a4b71dced760856049ee269e66ace0ec
yektt <yektaturan.93@gmail.com>
2021-04-13 17:16:34 +0200

    Created a test to singing up in user s-test

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,13 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "User"
-  # end
+  test 'singing up' do
+    visit(signup_path(:en))
+    fill_in(:user_email, with: 'test@mail.com')
+    fill_in(:user_password, with: 'password')
+    find(:button, 'Sign up').click
+
+    user = User.last
+    assert_equal current_path, edit_account_path(:en, user)
+  end
 end
