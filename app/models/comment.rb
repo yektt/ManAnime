e1860b4e44c5222ef7d1591306e3a404a1f7b8a5
yektt <yektaturan.93@gmail.com>
2021-04-13 15:00:52 +0200

    Added validations to comment and reply models
    for length of the bodies of the model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,5 +6,5 @@ class Comment < ApplicationRecord
   has_many :votes, dependent: :destroy
   has_and_belongs_to_many :users, dependent: :destroy
 
-  validates :comment_body, presence: true
+  validates :comment_body, presence: true, length: { minimum: 5 }
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,5 +1,5 @@
 class Reply < ApplicationRecord
-  validates :reply_body, presence: true
+  validates :reply_body, presence: true, length: { minimum: 5 }
 
   belongs_to :comment
   belongs_to :user
