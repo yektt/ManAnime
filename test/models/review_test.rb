1b34f9114543b9b1240e5666e455fa8767f3b2e1
yektt <yektaturan.93@gmail.com>
2021-04-13 16:22:31 +0200

    Creating a test to create review w/o body in review m-test

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -35,4 +35,18 @@ class ReviewTest < ActiveSupport::TestCase
     review = Review.new review_body:'Review body here', rating: 8, user: user, content: content
     refute review.valid?
   end
+
+  test 'creating review without body' do
+    content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
+                      tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
+                      image:'https://pbs.twimg.com/media/EGUS_PzWwAAStTa.jpg',
+                      volume_or_season_number:2, episode_or_chapter_number: 35
+    content.save!
+
+    user = User.new email:'test@mail.com', name:'Test user', password:'password'
+    user.save!
+
+    review = Review.new categories:'General', rating: 8, user: user, content: content
+    refute review.valid?
+  end
 end
