db7c161d86ae748dfa12dee800d330f0035d3fda
yektt <yektaturan.93@gmail.com>
2021-04-13 15:06:36 +0200

    Created a test to create comment w/o body in comment m-test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -19,6 +19,20 @@ class CommentTest < ActiveSupport::TestCase
     assert_equal content.comments.last, comment
   end
 
+  test 'creating a comment without body' do
+    content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
+                          tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
+                          image:'https://pbs.twimg.com/media/EGUS_PzWwAAStTa.jpg',
+                          volume_or_season_number:2, episode_or_chapter_number: 35
+    content.save!
+
+    user = User.new email:'test@mail.com', name:'Test user', password:'password'
+    user.save!
+
+    comment = Comment.new user: user, content: content
+    refute comment.valid?
+  end
+
   test 'creating more than one comment and assinging to a content' do
     content = Content.new name:'Content', start_date:'01/02/2003', category:'anime',
                       tags:'tag1, tag2', description:'He was very little when he lost both of his parents. He decided to protect weaks after that....',
