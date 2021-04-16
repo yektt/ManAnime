4f4952ab9f3b2559923f59dcdc5a41c4a06e2bd8
yektt <yektaturan.93@gmail.com>
2021-04-16 17:20:07 +0200

    Added mailer configuration to the production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,21 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "ManAnime_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+    # Don't care if the mailer can't send.
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'localhost:3000',
+      user_name:            'mananimeteam@gmail.com',
+      password:             'fUgpic-zeqpyx-hozju0',
+      authentication:       'plain',
+      enable_starttls_auto: true  
+    }
+    
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
