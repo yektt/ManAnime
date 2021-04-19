57561339204f3e356a713cc541543c7b09bf0280
yektt <yektaturan.93@gmail.com>
2021-04-19 11:39:24 +0200

    Fixed the intege

--- db/migrate/20210218141042_add_rating_to_content.rb
+++ db/migrate/20210218141042_add_rating_to_content.rb
@@ -1,6 +1,6 @@
 class AddRatingToContent < ActiveRecord::Migration[5.1]
   def change
     add_column :contents, :rating, :integer
-    add_column :contents, :rating_number, :intege
+    add_column :contents, :rating_number, :integer
   end
 end
