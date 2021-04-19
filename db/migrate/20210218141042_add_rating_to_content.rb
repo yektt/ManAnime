682854eabe1f13a1aa50343ef4c9e4891ae0287f
yektt <yektaturan.93@gmail.com>
2021-04-19 11:40:08 +0200

    Revert "Fixed the intege"
    This reverts commit 57561339204f3e356a713cc541543c7b09bf0280.

--- db/migrate/20210218141042_add_rating_to_content.rb
+++ db/migrate/20210218141042_add_rating_to_content.rb
@@ -1,6 +1,6 @@
 class AddRatingToContent < ActiveRecord::Migration[5.1]
   def change
     add_column :contents, :rating, :integer
-    add_column :contents, :rating_number, :integer
+    add_column :contents, :rating_number, :intege
   end
 end
