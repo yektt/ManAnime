6f9270fbea3cfb9ecf688209b12eb2d9ec027edd
yektt <yektaturan.93@gmail.com>
2021-04-23 16:40:49 +0200

    Fixed configuration for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'https://fast-mesa-32880.herokuapp.com',
+    domain:               'mananime.herokuapp.com',
     user_name:            'mananimeteam@gmail.com',
     password:             'blldobjkqcqxetfy',
     authentication:       'plain',
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
